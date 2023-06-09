01bba91fbc6bf679ced60ed61138cd6ba47853a1
Alex Kozik <ajk333@cornell.edu>
2023-06-09 19:42:28 -0400

    Sign Up page

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,33 +1,55 @@
+import { useEffect } from "react"
 import Nav from "./components/Nav"
+import "./signup.css"
 
 export default function SignUpSignIn () {
+
+
+    useEffect(() => {
+        const signupBoxButton = document.getElementById("signup-box-button")
+        signupBoxButton?.addEventListener("mouseover", () => {
+            signupBoxButton.classList.remove("signup-box-button-no-hover")
+            signupBoxButton.classList.add("signup-box-button-hover")
+        })
+        signupBoxButton?.addEventListener("mouseout", () => {
+            signupBoxButton.classList.remove("signup-box-button-hover")
+            signupBoxButton.classList.add("signup-box-button-no-hover")
+        })
+
+        const inputs = document.getElementsByClassName("signup-box-input")
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener("focus", () => {
+               
+                inputs[i].classList.remove("signup-box-input-not-selected")
+                inputs[i].classList.add("signup-box-input-selected")
+            })
+            inputs[i].addEventListener("blur", () => {
+                inputs[i].classList.remove("signup-box-input-selected")
+                inputs[i].classList.add("signup-box-input-not-selected")
+            })
+
+         
+        }
+    })
 
     return (
         <>
         <Nav/>
-            <div
-            style = {{height: "15vh"}}
-            ></div>
-          <div className = "flex flex-col items-center"
-          
-          style = {{height: "80vh"}}>
-
-           
-                <div id = "sign-up-box" className = "text-5xl  text-center border-2 border-solid border-black rounded-md">
-                    
-                    <h1>Sign Up</h1>
-
-                <div id = "sign-up-fields" className = "flex flex-col mt-10">
-                    <input className = "my-2 text-4xl border-4 rounded-md" type="text" placeholder="email"/>
-                    <input className = "my-2 text-4xl border-4 rounded-md" type="text" placeholder="password"/>
-                    <input className = "my-2 text-4xl border-4 rounded-md" type="text" placeholder="confirm password"/>
-                    <input className = "my-2 text-4xl border-4 rounded-md" type="text" placeholder="username"/>
-                    <button className = "mt-8 text-4xl border-4 rounded-md">Sign Up</button>
-
+            <span className = "signup-box">
+                <h1 className = "signup-box-header text-6xl">Sign Up</h1>
+                <div className = "signup-description text-2xl">
+                    To sign up, select a username and provide a valid email address. Then, create a secure password.
+                    Click the Sign Up button to proceed.
                 </div>
-            </div>
-    
-                </div>
+                <input className = "signup-box-input" type="text" placeholder="Username"></input>
+                <input className = "signup-box-input" type="text" placeholder="Email"></input>
+                <input className = "signup-box-input" type="text" placeholder="Password"></input>
+                <input className = "signup-box-input" type="text" placeholder="Confirm Password"></input>
+                <button id = "signup-box-button" className = "signup-box-button text-4xl"
+                
+                
+                >Sign Up</button>
+            </span>
         </>
     )
   }
