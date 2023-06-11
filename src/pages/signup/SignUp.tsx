88d30f8165257a9c9a64bc835966aae64ed697fa
Alex Kozik <ajk333@cornell.edu>
2023-06-10 20:28:50 -0400

    Sign up page

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -13,14 +13,21 @@ export default function SignUpSignIn () {
                     To sign up, select a username and provide a valid email address. Then, create a secure password.
                     Click the Sign Up button to proceed.
                 </div>
+                <hr className = "divider"/>
                 <input className = "signup-box-input" type="text" placeholder="Username"></input>
                 <input className = "signup-box-input" type="text" placeholder="Email"></input>
                 <input className = "signup-box-input" type="text" placeholder="Password"></input>
                 <input className = "signup-box-input" type="text" placeholder="Confirm Password"></input>
                 <button id = "signup-box-button" className = "signup-box-button"
+                // already have an account link
                 
                 
                 >Sign Up</button>
+                <hr className = "divider"/>
+                <div className = "already-have-account-container">
+                    <div className = "already-have-account-description">Already have an account?</div>
+                    <button className = "signin-button">Sign In</button>
+                </div>
             </span>
         </>
     )
