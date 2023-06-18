5821938c3495813f7bb80bf3b1863e21b4d2816a
Alex Kozik <ajk333@cornell.edu>
2023-06-18 16:41:18 -0400

    Make a vague template for the habits page

--- frontend/src/pages/Habits/Habits.tsx
+++ frontend/src/pages/Habits/Habits.tsx
@@ -1,9 +1,61 @@
 import Nav from "../../components/nav/Nav";
+import "./habits.css";
+
+
+
+function HabitsBar() {
+    return (
+        <div id = "habits-bar">
+            <div className = "habits-bar-link">
+                Habit Stack Creator
+            </div>
+            <div className = "habits-bar-link">
+                Habit Creator
+            </div>
+            <div className = "habits-bar-link">
+                Habit Resistor
+            </div>
+            <div className = "habits-bar-link">
+                Daily Rating
+            </div>
+
+
+        </div>
+    )
+}
+
+function HappinessGraph() {
+    return (
+        <div id = "happiness-graph">
+
+
+        </div>
+    )
+}
+
+function HabitsCalendar() {
+    return (
+        <div id = "habits-calendar">
+
+
+        </div>
+    )
+}
+
+function DailyRatingWindow() {
+
+}
+
+
 
 export default function Habits() {
     return (
         <>
-            <h1>Habits</h1>
+            <div className = "habits-container">
+                <HabitsBar />
+                <HappinessGraph />
+                <HabitsCalendar />
+            </div>
         </>
     )
 }
