98d728829b0598d1fbef67324c8c0f6562d425a9
Alex Kozik <ajk333@cornell.edu>
2023-06-19 07:26:59 -0400

    Created most of the UI for habit creator

--- frontend/src/pages/Habits/Habits.tsx
+++ frontend/src/pages/Habits/Habits.tsx
@@ -11,7 +11,9 @@ function HabitsBar() {
             <div className = "habits-bar-link">
                 Habit Stack Creator
             </div>
-            <div className = "habits-bar-link">
+            <div className = "habits-bar-link"
+            onClick = {openOrCloseHabitCreatorWindow}
+            >
                 Habit Creator
             </div>
             <div className = "habits-bar-link">
@@ -46,6 +48,132 @@ function DailyRatingWindowExitButton() {
            ✖️
         </div>
     )
+}
+
+function openOrCloseHabitCreatorWindow() {
+    if ($("#habit-creator-window").css("display") === "none") {
+        $("#habit-creator-window").css("display", "flex");
+        $("html").css("overflow", "hidden")
+    } else {
+        $("#habit-creator-window").css("display", "none");
+        $("html").css("overflow", "auto")
+    }
+}
+
+function ExitHabitCreatorWindowButton() {
+    return (
+        <div className = "habit-creator-window-exit-button"
+        onClick = {openOrCloseHabitCreatorWindow}
+        >
+           ✖️
+        </div>
+    )
+}
+
+function HabitCreatorWindow() {
+    /*
+    Habit Name
+    Habit Description
+    I will (action) at time (time) at location (location)
+    I will make it obvious by (obvious)
+    I will make it attractive by (attractive)
+    I will make it easy by (easy)
+    I will make it satisfying by (satisfying)
+
+    */
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [action, setAction] = useState("");
+    const [time, setTime] = useState("");
+    const [location, setLocation] = useState("");
+    const [obvious, setObvious] = useState("");
+    const [attractive, setAttractive] = useState("");
+    const [easy, setEasy] = useState("");
+    const [satisfying, setSatisfying] = useState("");
+
+    return (
+        <div id = "habit-creator-window">
+            <ExitHabitCreatorWindowButton />
+            <div className = "habit-creator-window-header">
+                Habit Creator
+            </div>
+            <input id = "habit-creator-window-name-input"
+            placeholder = "Habit Name"
+            >
+
+            </input>
+
+            <div className = "implementation-intention-header">
+                Implementation Intention
+            </div>
+
+            <div className = "implementation-intention-input-area">
+                <div className = "implementation-intention-sub-label">
+                    I will
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "action"
+                >
+                </input>
+                <div className = "implementation-intention-sub-label">
+                    at time
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "time"
+                >
+                </input>
+                <div className = "implementation-intention-sub-label">
+                    at location
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "location"
+                >
+                </input>
+            </div>
+
+            <div className = "the-laws-header">
+                    The Habit Change Laws
+                </div>
+            <div className = "the-laws-input-area">
+
+                
+                <div className = "implementation-intention-sub-label">
+                    I will make it obvious by
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "obvious"
+                >
+                </input>
+                <div className = "implementation-intention-sub-label">
+                    I will make it attractive by
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "attractive"
+                >
+                </input>
+                <div className = "implementation-intention-sub-label">
+                    I will make it easy by
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "easy"
+                >
+                </input>
+                <div className = "implementation-intention-sub-label">
+                    I will make it satisfying by
+                </div>
+                <input className = "implementation-intention-input"
+                placeholder = "satisfying"
+                >
+                </input>
+                
+            </div>
+            <div id = "create-habit-button">
+                Create Habit
+            </div>
+        </div>
+    )
+
+    
 }
 
 function DailyRatingWindow() {
@@ -94,8 +222,10 @@ function DailyRatingWindow() {
 function openOrCloseDailyRatingWindow() {
     if ($("#daily-rating-window").css("display") === "none") {
         $("#daily-rating-window").css("display", "flex");
+        $("html").css("overflow", "hidden")
     } else {
         $("#daily-rating-window").css("display", "none");
+        $("html").css("overflow", "auto")
     }
 }
 
@@ -124,6 +254,7 @@ export default function Habits() {
                 <HabitsCalendar />
                 <HappinessGraph />
                 <DailyRatingWindow />
+                <HabitCreatorWindow />
             </div>
         </>
     )
